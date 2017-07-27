987d1b2298ae1733c6f0faf69aa9cd9d173173d1
piRstone <pierre.lavalley@gmail.com>
2017-07-27 14:06:20 +0200

    options page : set label & interval

---
+++ js/options.js
@@ -0,0 +1,48 @@
+var apiKey = "e9e260718f3e80217b7d67cc20cd05a27019862c";
+var DEFAULT_INTERVAL = 15;
+
+// Display bikes count for favorite station
+function displayIconNumber(e) {
+    localStorage['bql-show-number'] = e.target.checked;
+    if (e.target.checked == true) {
+        chrome.browserAction.setBadgeText({text: "*"});
+        $('#show-interval').removeClass("hide")
+    } else {
+        chrome.browserAction.setBadgeText({text: ""});
+        $('#show-interval').addClass("hide");
+    }
+}
+
+// Set icon number refresh interval (in minutes)
+function setRefreshInterval() {
+    localStorage['bql-refresh-interval'] = $('#interval').val();
+    $('.interval-change-ok').removeClass("hide");
+    setTimeout(function() {
+        $('.interval-change-ok').addClass("hide");
+    }, 2000);
+}
+
+function init() {
+    // Set number checkbox value
+    if (localStorage['bql-show-number'] != undefined) {
+        if (JSON.parse(localStorage['bql-show-number']) == true) {
+            $('#show-icon-label').prop("checked", true);
+        } else {
+            $('#show-interval').addClass("hide");
+        }
+    }
+
+    // Set refresh interval
+    if (localStorage['bql-refresh-interval'] != undefined) {
+        var interval = JSON.parse(localStorage['bql-refresh-interval']);
+        $('#interval').val(interval);
+    } else {
+        $('#interval').val(DEFAULT_INTERVAL);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    init();
+    document.getElementById('show-icon-label').addEventListener('change', displayIconNumber);
+    $('.validate-interval').click(setRefreshInterval);
+});
