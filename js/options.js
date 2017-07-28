e74a5a8760e698a908da2a9c44a6974328bc06a2
piRstone <pierre.lavalley@gmail.com>
2017-07-29 01:25:43 +0200

    options configuration trajet préféré

--- js/options.js
+++ js/options.js
@@ -1,18 +1,56 @@
 var apiKey = "e9e260718f3e80217b7d67cc20cd05a27019862c";
 var DEFAULT_INTERVAL = 15;
 
+function initFavJournee(stations) {
+    // Fill selects options
+    var begin = $('#begin-station');
+    var end = $('#end-station');
+    for(var i=0 ; i < stations.length ; i++) {
+        begin.append('<option value="'+ stations[i].number +'" id="beg-'+ stations[i].number +'">'+ stations[i].name +'</options>');
+        end.append('<option value="'+ stations[i].number +'" id="end-'+ stations[i].number +'">'+ stations[i].name +'</options>');
+    }
+
+    // Set options if data are filled
+    if (localStorage['bql-beg-station'] != undefined && localStorage['bql-end-station'] != undefined) {
+        var begStationId = parseInt(localStorage['bql-beg-station']);
+        var endStationId = parseInt(localStorage['bql-end-station']);
+        var begOption = $('#beg-'+begStationId);
+        var endOption = $('#end-'+endStationId);
+        begOption.prop('selected', true);
+        endOption.prop('selected', true);
+    }
+}
+
 function getSavedStations() {
     var url = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey="+apiKey;
+    var favStation;
+    if (localStorage['bql-fav-station'] != undefined) {
+        favStation = localStorage['bql-fav-station'];
+    }
     if (localStorage['bql-fav-stations'] != undefined) {
         var savedStationsIds = localStorage['bql-fav-stations'].split(',');
+        var savedStations = [];
         $.get(url, function(data) {
             var select = $('#fav-stations-choice');
             for(var i=0 ; i < savedStationsIds.length ; i++) {
                 for(var j=0 ; j < data.length ; j++) {
                     if (data[j].number === parseInt(savedStationsIds[i])) {
-                        select.append('<option value="'+data[j].number+'">'+data[j].name+'</option>');
+                        // Add station to saved stations list
+                        savedStations.push(data[j]);
+
+                        // Fill the select options
+                        if (favStation && data[j].number == favStation) {
+                            select.append('<option value="'+data[j].number+'" selected>'+data[j].name+'</option>');
+                        } else {
+                            select.append('<option value="'+data[j].number+'">'+data[j].name+'</option>');
+                        }
                     }
                 }
+            }
+
+            // Init favorite journee
+            if (savedStations.length > 0) {
+                initFavJournee(savedStations);
             }
         });
     }
@@ -21,7 +59,7 @@ function getSavedStations() {
 function getStationAvailableBikesCount(stationId, success) {
     var url = "https://api.jcdecaux.com/vls/v1/stations/"+stationId+"?contract=nantes&apiKey="+apiKey;
     $.get(url, function(data) {
-        if (data.available_bikes) {
+        if (data.available_bikes != undefined) {
             success(data.available_bikes);
         } else {
             success('*');
@@ -58,6 +96,10 @@ function setRefreshInterval() {
     }, 2000);
 }
 
+function displayFavJournee(e) {
+    localStorage['bql-display-fav-journee'] = e.target.checked;
+}
+
 function saveFavStation() {
     var stationId = $('#fav-stations-choice').val();
     localStorage['bql-fav-station'] = stationId;
@@ -65,6 +107,24 @@ function saveFavStation() {
     setTimeout(function() {
         $('.fav-station-change-ok').addClass("hide");
     }, 2000);
+}
+
+function saveJournee() {
+    var begin = $('#begin-station').val();
+    var end = $('#end-station').val();
+    if (begin != end) {
+        localStorage['bql-beg-station'] = begin;
+        localStorage['bql-end-station'] = end;
+        $('.fav-journee-change-ok').removeClass("hide");
+        setTimeout(function() {
+            $('.fav-journee-change-ok').addClass("hide");
+        }, 2000);
+    } else {
+        $('.fav-journee-error').removeClass("hide");
+        setTimeout(function() {
+            $('.fav-journee-error').addClass("hide");
+        }, 4000);
+    }
 }
 
 function init() {
@@ -87,11 +147,20 @@ function init() {
     } else {
         $('#interval').val(DEFAULT_INTERVAL);
     }
+
+    // Set fav journee checkbox
+    if (localStorage['bql-display-fav-journee'] != undefined) {
+        if (JSON.parse(localStorage['bql-display-fav-journee']) == true) {
+            $('#show-fav-station').prop("checked", true);
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     init();
-    document.getElementById('show-icon-label').addEventListener('change', displayIconNumber);
+    $('#show-icon-label').change(displayIconNumber);
     $('.validate-interval').click(setRefreshInterval);
     $('.validate-fav-station').click(saveFavStation);
+    $('#show-fav-station').change(displayFavJournee);
+    $('.validate-fav-journee').click(saveJournee);
 });
